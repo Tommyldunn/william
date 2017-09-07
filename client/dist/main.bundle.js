fc3b28052c12f0d75cd378923320302c7f966988
tommyldunn <tommylogandunn@gmail.com>
2017-09-07 04:48:28 -0400

    dist

--- client/dist/main.bundle.js
+++ client/dist/main.bundle.js
@@ -501,14 +501,9 @@ module.exports = __webpack_require__(197);
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-// The file contents for the current environment will overwrite these during build.
 var environment = {
-    production: false,
-    wsUrl: 'ws://localhost:4040'
+    production: true,
+    wsUrl: 'wss://william-the-bot.herokuapp.com/'
 };
 //# sourceMappingURL=environment.js.map
 
